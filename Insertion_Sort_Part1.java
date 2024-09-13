df360293ae1041bfa8426ca37eea689fc5b3b1c7
NIKHIL RAGHUWANSHI <65480423+nikhilraghu@users.noreply.github.com>
2024-09-13 21:22:10 +0530

    Update Insertion_Sort_Part1.java

--- Insertion_Sort_Part1.java
+++ Insertion_Sort_Part1.java
@@ -1,13 +1,20 @@
-public static void insertionSort1(int n, List<Integer> arr) {
-    int key = arr.get(n - 1);
-    int i = n - 2;
-    
-    while (i >= 0 && arr.get(i) > key) {
-        arr.set(i + 1, arr.get(i));
-        System.out.println(arr.stream().map(String::valueOf).collect(Collectors.joining(" ")));
-        i--;
+    public static void insertionSort1(int n, List<Integer> arr) {
+        int valueToInsert = arr.get(n - 1);
+        int i = n - 2;
+        
+        while (i >= 0 && arr.get(i) > valueToInsert) {
+            arr.set(i + 1, arr.get(i)); /
+            printArray(arr); 
+            i--;
+        }
+        
+        arr.set(i + 1, valueToInsert);
+        printArray(arr); 
     }
     
-    arr.set(i + 1, key);
-    System.out.println(arr.stream().map(String::valueOf).collect(Collectors.joining(" ")));
-}
+    private static void printArray(List<Integer> arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
